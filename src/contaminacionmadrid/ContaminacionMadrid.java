4785d7caa7e13389d42aeec4f43fb3d963991b0e
webfuhrer <ataraxa@hotmail.com>
2018-10-04 13:24:46 +0200

    Primer commit

---
+++ src/contaminacionmadrid/ContaminacionMadrid.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contaminacionmadrid;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author luis
+ */
+public class ContaminacionMadrid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  ArrayList<Contaminante> lista=AccesoFichero.obtenerContaminantes();
+        
+    }
+    
+}
